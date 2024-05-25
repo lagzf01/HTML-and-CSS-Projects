f6cc4ff0d3ca932d563d3c7712f27cffdb7a3093
lagzf01 <162258280+lagzf01@users.noreply.github.com>
2024-05-25 10:54:36 +0100

    Initial commit

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js/Java_Alert.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<script>
+alert("Hello, World! I am an alert box!");
+    function My_First_Function (){
+        var string = "Kiss me, I am Irish";
+        var result = string.fontcolor("green");
+        document.getElementById("Irish").innerHTML = result;
+    }
+</script>
+</html>
---
+++ Basic_HTML_and_JavaScript_1/Hello_World.js
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html>
+<scrpt>
+alert("Hello, World! I am an alert box!");
+</script>
+</html>
